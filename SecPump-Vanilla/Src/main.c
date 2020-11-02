69ccb92e6df093b4a6895b9c825d41747b003ce1
r3glisss <cyrbresch@protonmail.com>
2020-11-02 09:40:37 +0100

    Adding License (9)

--- SecPump-Vanilla/Src/main.c
+++ SecPump-Vanilla/Src/main.c
@@ -1,19 +1,16 @@
-/* USER CODE BEGIN Header */
-/****************************************************************************
-  Copyright (C) 2019 LCIS Laboratory - Cyril Bresch
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, in version 3.
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
-  This program is part of the SecPump @https://github.com/r3glisss/SecPump
- ****************************************************************************
+// Copyright (C) 2019 LCIS Laboratory - Cyril Bresch
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, in version 3.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// This program is part of the SecPump @https://github.com/r3glisss/SecPump
 
- ******************************************************************************
+/******************************************************************************
  * @file           : main.c
  * @brief          : Main program body
  ******************************************************************************
@@ -29,7 +26,6 @@
  *
  ******************************************************************************
  */
-/* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
--- SecPump-Vuln/Src/main.c
+++ SecPump-Vuln/Src/main.c
@@ -1,17 +1,16 @@
-/* USER CODE BEGIN Header */
+// Copyright (C) 2019 LCIS Laboratory - Cyril Bresch
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, in version 3.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// This program is part of the SecPump @https://github.com/r3glisss/SecPump
+
 /****************************************************************************
-  Copyright (C) 2019 LCIS Laboratory - Cyril Bresch
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, in version 3.
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
-  This program is part of the SecPump @https://github.com/r3glisss/SecPump
- ****************************************************************************
  *
  ******************************************************************************
  * @file           : main.c
@@ -28,8 +27,7 @@
  *                             www.st.com/SLA0044
  *
  ******************************************************************************
- */
-/* USER CODE END Header */
+*/
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
